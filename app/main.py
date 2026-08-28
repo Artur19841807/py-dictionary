3d6b1f132f625e0a6592671fed7c9a5f04048e6f
Artur Neruk <anerukpfk@gmail.com>
2026-08-28 08:42:49 +0300

    Implement hash table with dynamic resizing and collision resolution

--- app/main.py
+++ app/main.py
@@ -1,45 +1,90 @@
-from typing import Any, Iterator
+from typing import Any, Iterator, Optional
 
 
 class Dictionary:
+    INITIAL_CAPACITY = 8
+    LOAD_FACTOR_THRESHOLD = 2 / 3
+
     def __init__(self, data: Any = None) -> None:
-        self._items: list[tuple[Any, Any]] = []
-        if data:
+        self._capacity = self.INITIAL_CAPACITY
+        self._table: list[Optional[list[tuple[Any, Any]]]] = [
+            None for _ in range(self._capacity)
+        ]
+        self._length = 0
+
+        if data is not None:
             self.update(data)
 
-    def _find_index(self, key: Any) -> int:
-        for index, (k, _) in enumerate(self._items):
-            if k == key:
-                return index
-        return -1
+    def _get_bucket_index(self, key: Any) -> int:
+        return hash(key) % self._capacity
+
+    def _resize(self) -> None:
+        old_table = self._table
+        self._capacity *= 2
+        self._table = [None for _ in range(self._capacity)]
+        self._length = 0
+
+        for bucket in old_table:
+            if bucket is not None:
+                for key, value in bucket:
+                    self[key] = value
 
     def __getitem__(self, key: Any) -> Any:
-        index = self._find_index(key)
-        if index == -1:
-            raise KeyError(f"Key '{key}' not found in Dictionary.")
-        return self._items[index][1]
+        index = self._get_bucket_index(key)
+        bucket = self._table[index]
+
+        if bucket is not None:
+            for k, v in bucket:
+                if k == key:
+                    return v
+
+        raise KeyError(f"Key '{key}' not found in Dictionary.")
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        index = self._find_index(key)
-        if index != -1:
-            self._items[index] = (key, value)
+        if (self._length + 1) / self._capacity > self.LOAD_FACTOR_THRESHOLD:
+            self._resize()
+
+        index = self._get_bucket_index(key)
+        bucket = self._table[index]
+
+        if bucket is None:
+            self._table[index] = [(key, value)]
+            self._length += 1
         else:
-            self._items.append((key, value))
+            for idx, (k, _) in enumerate(bucket):
+                if k == key:
+                    bucket[idx] = (key, value)
+                    return
+            bucket.append((key, value))
+            self._length += 1
 
     def __delitem__(self, key: Any) -> None:
-        index = self._find_index(key)
-        if index == -1:
-            raise KeyError(f"Key '{key}' not found in Dictionary.")
-        self._items.pop(index)
+        index = self._get_bucket_index(key)
+        bucket = self._table[index]
+
+        if bucket is not None:
+            for idx, (k, _) in enumerate(bucket):
+                if k == key:
+                    bucket.pop(idx)
+                    self._length -= 1
+                    return
+
+        raise KeyError(f"Key '{key}' not found in Dictionary.")
 
     def __len__(self) -> int:
-        return len(self._items)
+        return self._length
 
     def __iter__(self) -> Iterator[Any]:
-        return iter(key for key, _ in self._items)
+        for bucket in self._table:
+            if bucket is not None:
+                for key, _ in bucket:
+                    yield key
 
     def items(self) -> Iterator[tuple[Any, Any]]:
-        return iter(self._items)
+        for bucket in self._table:
+            if bucket is not None:
+                for item in bucket:
+                    yield item
 
     def update(self, other: Any = None, **kwargs: Any) -> None:
         if other is not None:
@@ -54,10 +99,12 @@ class Dictionary:
             self[key] = value
 
     def clear(self) -> None:
-        self._items.clear()
+        self._capacity = self.INITIAL_CAPACITY
+        self._table = [None for _ in range(self._capacity)]
+        self._length = 0
 
     def get(self, key: Any, default: Any = None) -> Any:
-        index = self._find_index(key)
-        if index != -1:
-            return self._items[index][1]
-        return default
+        try:
+            return self[key]
+        except KeyError:
+            return default
